8d4f7b68797bca5a255b9531b9bdd96efad51f81
Yanis Gerst <yanisgerst.pro@gmail.com>
2023-11-12 08:40:58 +0100

    use aws server

--- apps/server/api/server/server.go
+++ apps/server/api/server/server.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"encoding/json"
 	"fmt"
 	usersRouter "thingsCloneServer/api/endpoints/users"
 	userAuthRouter "thingsCloneServer/api/endpoints/usersAuth"
@@ -19,6 +20,28 @@ type Server struct {
 	listener      IListener
 }
 
+func helloWorldHandler(w http.ResponseWriter, r *http.Request) {
+	// Créer une structure pour le JSON
+	response := struct {
+		Message string `json:"message"`
+	}{
+		Message: "hello world",
+	}
+
+	// Convertir la structure en JSON
+	jsonResponse, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Erreur interne du serveur", http.StatusInternalServerError)
+		return
+	}
+
+	// Définir l'en-tête de contenu JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	// Écrire le JSON dans la réponse
+	w.Write(jsonResponse)
+}
+
 func NewServer(listenAddress string, store storage.Storer, listener IListener) *Server {
 	return &Server{listenAddress: listenAddress, store: store, listener: listener}
 }
@@ -27,6 +50,7 @@ func (server *Server) Start() error {
 	fmt.Print("Server listening on ", server.listenAddress+"\n")
 	mux := http.NewServeMux()
 
+	http.Handle("/", http.HandlerFunc(helloWorldHandler))
 	usersRouter.Use(mux, server.store)
 	usersTodoRouter.Use(mux, server.store)
 	usersTodosRouter.Use(mux, server.store)
--- apps/server/main.go
+++ apps/server/main.go
@@ -25,7 +25,6 @@ func init() {
 }
 
 func main() {
-
 	listener := gateway.ListenAndServe
 	store := storage.NewStorer()
 	apiAdress := os.Getenv("URL")
